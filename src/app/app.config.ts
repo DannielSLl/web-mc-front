b0b546f9be133a18f82f24e4af25430676c49264
EduardoATito <eduardotito.jose01@gmail.com>
2024-04-26 14:34:15 -0400

    routerlink on top

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { InMemoryScrollingFeature, InMemoryScrollingOptions, provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled',
 };
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature = withInMemoryScrolling(scrollConfig);
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes, inMemoryScrollingFeature), provideHttpClient()]
+};
+
+
