b138880e404b2acb927b42097c7f443d92c54ff3
DannielSLl <daniel.supanta.llano@alumnos.uta.cl>
2024-05-26 20:14:33 -0400

    feat: componente de auth para la logistica

--- src/app/admin/routes.ts
+++ src/app/admin/routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
   {
     path: 'orders',
     loadComponent: () => import('./orders/orders.component').then(m => m.OrdersComponent),
+  },
+  {
+    path: 'products',
+    loadComponent: () => import('./products/products.component').then(m => m.ProductsComponent),
   }
 ];
