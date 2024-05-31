6cf3daa61fdb88056db0ed8e6ed9d1bfb92d942c
DannielSLl <daniel.supanta.llano@alumnos.uta.cl>
2024-05-30 23:26:51 -0400

    fix: error al importar enviroment

--- src/app/services/auth/auth-admin.service.ts
+++ src/app/services/auth/auth-admin.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { JwtTokenDto } from '../../model/token.dto';
 import { LoginUserDto } from '../../model/login-user.dto';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
