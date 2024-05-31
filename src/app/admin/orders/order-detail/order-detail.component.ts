0529c6ce5f36cbcc60109085ea4f02d19f84a794
DannielSLl <daniel.supanta.llano@alumnos.uta.cl>
2024-05-30 23:14:31 -0400

    feat: funcionalidad de detalle de ordenes

--- src/app/admin/orders/order-detail/order-detail.component.ts
+++ src/app/admin/orders/order-detail/order-detail.component.ts
@@ -3,10 +3,8 @@ import {
   EventEmitter,
   Input,
   OnChanges,
-  OnInit,
   Output,
   SimpleChanges,
-  ViewChild,
 } from '@angular/core';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { OrderDetailDto } from '../../../model/order-detail.dto';
@@ -19,46 +17,13 @@ import { OrdersService } from '../../../services/orders.service';
   templateUrl: './order-detail.component.html',
   styleUrl: './order-detail.component.css',
 })
-export class OrderDetailComponent implements OnChanges, OnInit {
+export class OrderDetailComponent implements OnChanges {
   @Output() viewChange: EventEmitter<boolean> = new EventEmitter<boolean>();
   @Input() id!: number;
 
   order!: OrderDetailDto;
 
   constructor(private orderService: OrdersService) {}
-  ngOnInit(): void {
-    this.order = {
-      id: 1,
-      nameClient: 'John Doe',
-      lastnameClient: 'Doe',
-      phone: '123456789',
-      dateOrder: '2021-09-01T00:00:00.000Z',
-      orderDeliveryDate: '2021-09-02T00:00:00.000Z',
-
-      status: 'pendiente',
-      total: 100,
-      products: [
-        {
-          name: 'Item 1',
-          quantity: 1,
-          price: 10,
-          totalPartial: 10,
-        },
-        {
-          name: 'Item 2',
-          quantity: 2,
-          price: 20,
-          totalPartial: 40,
-        },
-        {
-          name: 'Item 3',
-          quantity: 3,
-          price: 30,
-          totalPartial: 90,
-        },
-      ],
-    };
-  }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.getOrderDetail(changes['id'].currentValue);
@@ -79,7 +44,6 @@ export class OrderDetailComponent implements OnChanges, OnInit {
   Close() {
     this.viewChange.emit(false);
   }
-
 
   private getOrderDetail(id: number) {
     this.orderService.getOrderDetail(id).subscribe({
--- src/app/admin/orders/orders.component.ts
+++ src/app/admin/orders/orders.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 import { OrderListDto } from '../../model/order-list.dto';
 import { OrdersService } from '../../services/orders.service';
 import { OrderDetailComponent } from './order-detail/order-detail.component';
@@ -11,7 +11,6 @@ import { OrderDetailComponent } from './order-detail/order-detail.component';
   imports: [OrderDetailComponent],
 })
 export class OrdersComponent implements OnInit {
-
   @Input() ViewDetail!: boolean;
   @Output() selectedOrder: number | null = null;
 
@@ -20,30 +19,7 @@ export class OrdersComponent implements OnInit {
   constructor(private orderService: OrdersService) {}
 
   ngOnInit(): void {
-    //this.getOrders();
-    this.orders = [
-      {
-        id: 1,
-        client: 'John Doe',
-        date: new Date(),
-        items: 3,
-        status: 'pendiente',
-      },
-      {
-        id: 1,
-        client: 'John Doe',
-        date: new Date(),
-        items: 3,
-        status: 'pendiente',
-      },
-      {
-        id: 1,
-        client: 'John Doe',
-        date: new Date(),
-        items: 3,
-        status: 'pendiente',
-      },
-    ];
+    this.getOrders();
   }
 
   getOrders() {
@@ -65,7 +41,4 @@ export class OrdersComponent implements OnInit {
       this.selectedOrder = null;
     }
   }
-
-  markAsCompleted(arg0: any) {}
-
 }
